20bd12b1d4686872e415e3621f7c0035c05a6f3e
reimuLy <41979703+reimuLy@users.noreply.github.com>
2024-06-28 16:42:04 +0800

    Update mirror-images.sh

--- mirror-images.sh
+++ mirror-images.sh
@@ -3,7 +3,7 @@ source ECR_Auth.sh
 source mirror-base.sh
 IMAGES_FILE_LIST='required-images.txt'
 
-repos=$(grep -v ^# $IMAGES_FILE_LIST | sed 's/^[[:space:]]*//' | sed 's/\r//g')
+repos=$(grep -v ^# $IMAGES_FILE_LIST | cut -d: -f1 | sed 's/^[[:space:]]*//' | sed 's/\r//g' | sort -u)
 for repo in ${repos[@]}
 do
   replaceDomainName $repo
