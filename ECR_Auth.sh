2bdb44ea19f32484880c003a10e8caf2f5b23b86
reimuLy <41979703+reimuLy@users.noreply.github.com>
2024-07-01 14:22:12 +0800

    Update ECR_Auth.sh

--- ECR_Auth.sh
+++ ECR_Auth.sh
@@ -3,4 +3,4 @@ env
 
 aws configure --profile=ChinaECR set aws_access_key_id $ecr_ak
 aws configure --profile=ChinaECR set aws_secret_access_key $ecr_sk
-aws configure --profile=ChinaECR set default.region cn-northwest-1
+aws configure --profile=ChinaECR set default.region $ecr_region
